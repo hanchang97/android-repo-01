ba1d9c803da3f2332dad4d7a55c23cbd2166268e
한창희 <nimok97@gmail.com>
2022-07-13 10:34:30 +0900

    [FEAT] #4 #5 - 메인화면 탭 구현 및 프래그먼트 연결 TabLayout custom Issue, Notifiactions Fragment 이동 가능하게 연결

--- app/src/main/java/com/repo01/repoapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/repo01/repoapp/ui/main/MainActivity.kt
@@ -2,19 +2,25 @@ package com.repo01.repoapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayoutMediator
 import com.repo01.repoapp.R
 import com.repo01.repoapp.databinding.ActivityMainBinding
+import com.repo01.repoapp.ui.main.adapter.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val tabTitles = arrayListOf("Issue", "Notifications")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         initView()
+        setTabLayoutWithViewPager()
     }
 
     private fun initView(){
@@ -24,6 +30,18 @@ class MainActivity : AppCompatActivity() {
 
         binding.ivProfile.setOnClickListener {
 
+        }
+    }
+
+    private fun setTabLayoutWithViewPager() {
+        binding.vpTab.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(binding.tlSelect, binding.vpTab) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
+        for (i in 0..2) {
+            val textView = LayoutInflater.from(this).inflate(R.layout.tab_main_title, null) as TextView
+            binding.tlSelect.getTabAt(i)?.customView = textView
         }
     }
 }
---
+++ app/src/main/java/com/repo01/repoapp/ui/main/tab/notifications/NotificationsFragment.kt
@@ -0,0 +1,26 @@
+package com.repo01.repoapp.ui.main.tab.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.repo01.repoapp.R
+import com.repo01.repoapp.databinding.FragmentIssueBinding
+import com.repo01.repoapp.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+
+    private lateinit var binding: FragmentNotificationsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_notifications, container, false)
+        return binding.root
+    }
+}
