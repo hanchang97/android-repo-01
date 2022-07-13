7257f7cde512ae2832880a8b3ac6f066fcf501aa
Jihye Joo <oreocube13@gmail.com>
2022-07-13 15:54:48 +0900

    [DESIGN] #14 - 검색 화면 기본 레이아웃
    - Search Bar 추가
    - selector로 focus 상태에 따라 구분

---
+++ app/src/main/java/com/repo01/repoapp/ui/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.repo01.repoapp.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.repo01.repoapp.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
