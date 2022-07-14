b1d4d741987d78cf8a8bbe216b7be6d9204b4e43
Jihye Joo <oreocube13@gmail.com>
2022-07-14 17:52:06 +0900

    [FEAT] #19 - 프로필 조회 결과 보여주기
    - 데이터 바인딩 적용

--- app/src/main/java/com/repo01/repoapp/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/repo01/repoapp/ui/profile/ProfileActivity.kt
@@ -2,11 +2,37 @@ package com.repo01.repoapp.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.repo01.repoapp.R
+import com.repo01.repoapp.databinding.ActivityProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+    private val viewModel: ProfileViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
+
+        initToolbar()
+        observeData()
+    }
+
+    private fun initToolbar() {
+        binding.toolbar.setNavigationOnClickListener { finish() }
+    }
+
+    private fun observeData() {
+        viewModel.userInfo.observe(this) {
+            binding.userInfo = it
+            binding.layoutProfileContainer.repoCount = it.repoCount
+        }
+
+        viewModel.starredCount.observe(this) {
+            binding.layoutProfileContainer.starredCount = it
+        }
     }
 }
