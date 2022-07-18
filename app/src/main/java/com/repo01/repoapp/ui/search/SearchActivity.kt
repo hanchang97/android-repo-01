5bdc73030572162eae936b4fdd0ce462419f4def
Jihye Joo <oreocube13@gmail.com>
2022-07-18 16:34:50 +0900

    [FEAT] #17 - 검색 결과 페이징 및 소프트키보드

--- app/src/main/java/com/repo01/repoapp/ui/search/SearchActivity.kt
+++ app/src/main/java/com/repo01/repoapp/ui/search/SearchActivity.kt
@@ -1,14 +1,14 @@
 package com.repo01.repoapp.ui.search
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.repo01.repoapp.R
 import com.repo01.repoapp.databinding.ActivitySearchBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,17 +18,22 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
     private val adapter by lazy { SearchItemAdapter() }
     private val viewModel: SearchViewModel by viewModels()
+    private val inputMethodManager by lazy {
+        getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
-
         initViews()
         observeData()
     }
 
     private fun initViews() {
         binding.toolbar.setNavigationOnClickListener { finish() }
+        binding.clSearch.setOnClickListener {
+            binding.clSearch.requestFocus()
+        }
         initSearchEditText()
         initRecyclerView()
     }
@@ -42,9 +47,17 @@ class SearchActivity : AppCompatActivity() {
                     if (hasFocus) R.drawable.ic_variant10 else 0,
                     0
                 )
+                if (hasFocus.not()) {
+                    inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
+                }
             }
+            requestFocus()
             doAfterTextChanged {
-                viewModel.searchRepositoriesByQuery(it.toString())
+                if (it.toString().isEmpty()) {
+                    hideSearchResultRecyclerView()
+                } else {
+                    viewModel.searchRepos(it.toString())
+                }
             }
         }
     }
@@ -56,18 +69,13 @@ class SearchActivity : AppCompatActivity() {
                     DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
                 )
             }
-            layoutManager = LinearLayoutManager(context)
         }
     }
 
     private fun observeData() {
-        viewModel.searchResult.observe(this) {
-            if (it.isNotEmpty()) {
-                adapter.submitList(it)
-                showSearchResultRecyclerView()
-            } else {
-                hideSearchResultRecyclerView()
-            }
+        viewModel.result.observe(this) {
+            adapter.submitData(lifecycle, it)
+            showSearchResultRecyclerView()
         }
     }
 
